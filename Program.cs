e12599df8f39a4463b7b32d4a17633bfa8e973a6
Leovianaf <leonardoviana.nave@gmail.com>
2024-01-04 09:56:02 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testes com o Smartphone Nokia:");
+Smartphone nokia = new Nokia("2014", "Nokia 3310", "123456789", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Testes com o Smartphone Iphone:");
+Smartphone iphone = new Iphone("8708", "Iphone XR", "987654321", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Discord");
